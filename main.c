b8d033f4e8aa8c7c30000751c389eed85261fc1b
Marco <marco.mangan@gmail.com>
2024-04-02 14:06:00 +0000

    Adds kata prototype.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char *decode(const unsigned char *code, size_t n, unsigned key)
+{
+    return NULL;
+}
+
+int main()
+{
+    printf("TESTE\n");
+    return 0;
+}
