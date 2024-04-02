97a7c5bf3850664095effa57c1d9de88c13856d4
Marco <marco.mangan@gmail.com>
2024-04-02 14:10:58 +0000

    Adds solution outline

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 
 char *decode(const unsigned char *code, size_t n, unsigned key)
 {
+    // itoa on key
+    // keys counts from k - ´0´
+    // codes decoded as get(c - k)
+    // codes in an array, starting  at ´a´ on a[0]
     return NULL;
 }
 
